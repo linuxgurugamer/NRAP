92673c8a392370f0cd1a6117d81127e1a94c12dc
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-02-04 23:27:35 -0500

    Updates for release

---
+++ NRAP/buildRelease.bat
@@ -0,0 +1,30 @@
+﻿
+set RELEASEDIR=d:\Users\jbb\release
+set ZIP="c:\Program Files\7-zip\7z.exe"
+
+copy bin\Release\NRAP.dll ..\Output\GameData\NRAP\Plugins
+
+set VERSIONFILE=a.version
+copy D:\Users\jbb\github\NRAP\Output\GameData\NRAP\NRAP.version a.version
+rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
+c:\local\jq-win64  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
+set /P major=<tmpfile
+
+c:\local\jq-win64  ".VERSION.MINOR"  %VERSIONFILE% >tmpfile
+set /P minor=<tmpfile
+
+c:\local\jq-win64  ".VERSION.PATCH"  %VERSIONFILE% >tmpfile
+set /P patch=<tmpfile
+
+c:\local\jq-win64  ".VERSION.BUILD"  %VERSIONFILE% >tmpfile
+set /P build=<tmpfile
+del tmpfile
+set VERSION=%major%.%minor%.%patch%
+if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
+del a.version
+
+cd ../Output
+set FILE="%RELEASEDIR%\NRAP-%VERSION%.zip"
+IF EXIST %FILE% del /F %FILE%
+%ZIP% a -tzip %FILE% Gamedata\NRAP
+pause
---
+++ NRAP/deploy.bat
@@ -0,0 +1,5 @@
+﻿
+
+copy bin\Debug\NRAP.dll ..\Output\GameData\NRAP\Plugins
+
+xcopy /y/s ..\Output\GameData\NRAP R:\KSP_1.2.2_dev\GameData\NRAP
